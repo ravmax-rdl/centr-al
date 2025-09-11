538d7bd3748997f177b1d5078d47b27df71b0cf8
ravmax-rdl <ravindudl.rc@gmail.com>
2025-09-11 19:15:38 +0530

    feat(authors): enhance email verification and password reset functionality with dynamic email templates

--- src/collections/Authors/index.ts
+++ src/collections/Authors/index.ts
@@ -1,4 +1,5 @@
 import { isAdminOrModerator } from '@/access/isAdminorModerator';
+import { getServerSideURL } from '@/utilities/getURL';
 import type { CollectionConfig } from 'payload';
 
 export const Authors: CollectionConfig = {
@@ -16,7 +17,28 @@ export const Authors: CollectionConfig = {
   },
   auth: {
     tokenExpiration: 12 * 60 * 60,
-    verify: true, // Only verify email if SMTP is configured
+    verify: {
+      generateEmailSubject: (args) => {
+        return `Hey ${args.user.firstName ? args.user.firstName : args.user.email}, verify your email address`;
+      },
+      generateEmailHTML: (args) => {
+        return `<div><h1>Hey ${args.user.firstName ? args.user.firstName : args.user.email}!</h1>
+        <br/>
+        <p>Please verify your email address by going to <a href="${getServerSideURL()}/verify?token=${args.token}">${getServerSideURL()}/verify?token=${args.token}</a></p>
+        </div>`;
+      },
+    }, // Only verify email if SMTP is configured
+    forgotPassword: {
+      generateEmailSubject: (args) => {
+        return `Hey ${args?.user.firstName ? args?.user.firstName : args?.user.email}, reset your password`;
+      },
+      generateEmailHTML: (args) => {
+        return `<div><h1>Hey ${args?.user.firstName ? args?.user.firstName : args?.user.email}!</h1>
+        <br/>
+        <p>You (or someone) requested to reset your password. If this wasn't you, you can safely ignore this email. Otherwise, reset your password by going to <a href="${getServerSideURL()}/password-reset?token=${args?.token}">${getServerSideURL()}/password-reset?token=${args?.token}</a></p>
+        </div>`;
+      },
+    }, // Only verify email if SMTP is configured
     cookies: {
       secure: true,
       sameSite: 'None',
@@ -39,12 +61,6 @@ export const Authors: CollectionConfig = {
         },
       ],
     },
-    {
-        name: 'email',
-        type: 'email',
-        required: true,
-        unique: true,
-    }
   ],
   timestamps: true,
 };
