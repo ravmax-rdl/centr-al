1df56ba8061d55b55fcaad0d67086e06b92f7cf6
ravmax-rdl <ravindudl.rc@gmail.com>
2025-09-11 19:19:26 +0530

    feat(forgot-password): implement forgot password functionality with form and API integration for author accounts

---
+++ src/app/(frontend)/(account)/forgot-password/actions/forgotPassword.ts
@@ -0,0 +1,25 @@
+'use server';
+
+import { getPayload } from 'payload';
+import config from '@payload-config';
+import { Response } from '../../create-account/actions/create';
+
+export async function ForgotPassword({ email }: { email: string }): Promise<Response> {
+  const payload = await getPayload({ config });
+
+  try {
+    await payload.forgotPassword({
+      collection: 'authors',
+      data: { email },
+    });
+  } catch (error) {
+    console.log('Error in forgot password:', error);
+    return {
+      success: false,
+      error: 'An error occurred while processing your request. Please try again later.',
+    };
+  }
+  return {
+    success: true,
+  };
+}
---
+++ src/app/(frontend)/(account)/forgot-password/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ForgotForm from './components/ForgotPasswordForm';
+
+export const Page = () => {
+  return (
+    <div className="h-[100vh] w-full mx-auto sm:max-w-sm">
+      <div className="flex justify-center mt-8">
+        <ForgotForm />
+      </div>
+    </div>
+  ); 
+};
+
+export default Page;
