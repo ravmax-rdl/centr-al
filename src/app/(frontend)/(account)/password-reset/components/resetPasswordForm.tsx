20acb53326ea0f15a8b3faf31e9fd926c13542f8
ravmax-rdl <ravindudl.rc@gmail.com>
2025-09-11 19:20:36 +0530

    feat(password-reset): implement password reset functionality with form handling and user feedback for authors

---
+++ src/app/(frontend)/(account)/password-reset/components/resetPasswordForm.tsx
@@ -0,0 +1,56 @@
+'use client';
+
+import React, { ReactElement, useState } from 'react';
+import { useRouter } from 'next/navigation';
+import SubmitButton from '@/components/AuthorForm/SubmitButton';
+import { Input } from '@/components/AuthorForm/Input';
+import { FormContainer } from '@/components/AuthorForm/FormContainer';
+import { resetPassword } from '../actions/resetPassword';
+import { Response } from '../../create-account/actions/create';
+import { set } from 'react-hook-form';
+
+export default function ResetForm({ token }: { token: string }): ReactElement {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState<string | null>(null);
+  const router = useRouter();
+
+  async function handleSubmit(event: React.FormEvent<HTMLFormElement>): Promise<void> {
+    event.preventDefault();
+    setIsLoading(true);
+    setError(null);
+
+    const formData = new FormData(event.currentTarget);
+
+    const password = formData.get('password') as string;
+    const confirmPassword = formData.get('confirmPassword') as string;
+
+    if (password !== confirmPassword) {
+      setError('Passwords do not match.');
+      setIsLoading(false);
+      return;
+    }
+
+    const result: Response = await resetPassword({ token, password });
+
+    setIsLoading(false);
+
+    if (result.success) {
+      router.push(`/login?${encodeURIComponent('Password reset successful. Please log in.')}`);
+    } else {
+      setError(result.error || 'An error occurred while resetting your password.');
+    }
+  }
+
+  return (
+    <FormContainer heading="Reset Your Password">
+      <div className="w-full mx-auto sm:max-w-sm">
+        <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
+          <Input name="password" type="password" label="Password" />
+          <Input name="confirmPassword" type="password" label="Confirm Password" />
+          {error && <div className="text-red-400">{error}</div>}
+          <SubmitButton loading={isLoading} text="Reset Password" />
+        </form>
+      </div>
+    </FormContainer>
+  );
+}
---
+++ src/app/(frontend)/(account)/password-reset/page.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { getUser } from '../../(auth)/actions/getUser';
+import { redirect } from 'next/navigation';
+import ResetForm from './components/resetPasswordForm';
+import NotFound from '../../not-found';
+
+interface SearchParams {
+  [key: string]: string | undefined;
+}
+
+export default async function Page({
+  searchParams,
+}: {
+  searchParams: SearchParams;
+}): Promise<React.ReactElement> {
+  const user = await getUser();
+  if (user) {
+    redirect('/dashboard');
+  }
+  const { message, token } = await searchParams;
+
+  if (token) {
+    return (
+      <div className="h-[100vh] w-full mx-auto sm:max-w-sm">
+        <div className="flex justify-center mt-8">
+          {message && (
+            <p className="w-auto inline-block mx-auto p-4 bg-emerald-100 text-emerald-950 border-e-emerald-950 rounded-md border">
+              {message}
+            </p>
+          )}
+        </div>
+        <ResetForm token={token} />
+      </div>
+    );
+  } else {
+    redirect(`/login?message=${encodeURIComponent('No reset token found')}`);
+  }
+}
