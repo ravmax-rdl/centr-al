8ac7360fa7945908599ff1b84919552c3c3fe313
ravmax-rdl <ravindudl.rc@gmail.com>
2025-11-24 17:53:22 +0530

    refactor(categories): move into a collection folder

--- src/collections/Categories/index.ts
+++ src/collections/Categories/index.ts
@@ -3,12 +3,11 @@ import type { CollectionConfig } from 'payload';
 import { anyone } from '@/access/anyone';
 import { isAdminOrModerator } from '@/access/isAdminorModerator';
 import { slugField } from '@/fields/slug';
-import { isAdmin } from '@/access/isAdmin';
 
 export const Categories: CollectionConfig = {
   slug: 'categories',
   access: {
-    create: isAdmin,
+    create: isAdminOrModerator,
     delete: isAdminOrModerator,
     read: anyone,
     update: isAdminOrModerator,
