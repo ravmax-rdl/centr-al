6a61aa99a9650606832a7ef75f6c98cfea256545
ravmax-rdl <ravindudl.rc@gmail.com>
2025-11-24 22:39:20 +0530

    refactor(FormContainer): simplify heading styling in the form container

--- src/components/UserForm/FormContainer.tsx
+++ src/components/UserForm/FormContainer.tsx
@@ -10,7 +10,7 @@ export const FormContainer = ({
   return (
     <div className="flex min-h-full flex-col justify-center py-12">
       <div className="mb-10">
-        <h1 className="text-3xl font-semibold text-center tracking-tight">{heading}</h1>
+        <h1 className="text-3xl text-center">{heading}</h1>
       </div>
       <div className="w-full mx-auto sm:max-w-md">{children}</div>
     </div>
