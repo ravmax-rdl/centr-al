713c3eb86fa0cd256c22941c3fce9a726cec1e06
ravmax-rdl <ravindudl.rc@gmail.com>
2025-11-26 13:33:12 +0530

    refactor(Template): update import path for LogoutButton component

--- src/app/(frontend)/(auth)/template.tsx
+++ src/app/(frontend)/(auth)/template.tsx
@@ -1,7 +1,7 @@
 import { redirect } from 'next/navigation';
 import React, { ReactNode } from 'react';
 import { getUser } from './actions/getUser';
-import { LogoutButton } from './components/LogoutButton';
+import { LogoutButton } from '@/components/LogoutButton';
 
 const Template: React.FC<{ children: ReactNode }> = async ({ children }) => {
   const user = await getUser();
